d489952f917765ce3d24fca2236b63d1e0e83f41
Shobhit Garg <gargshobhit90@gmail.com>
2018-01-09 17:04:31 -0800

    Change organization name (periscopedata) to lowercase

--- push_docker.sh
+++ push_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 IMAGE_ID=$1
-docker tag $IMAGE_ID PeriscopeData/concourse-multiple-kube-deployments:latest
-docker push PeriscopeData/concourse-multiple-kube-deployments:latest
+docker tag $IMAGE_ID periscopedata/concourse-multiple-kube-deployments:latest
+docker push periscopedata/concourse-multiple-kube-deployments:latest
