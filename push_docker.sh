9b16e9cb32f23f6b6e8b8a6e09e8587026a8f70d
Shobhit Garg <gargshobhit90@gmail.com>
2018-01-10 13:17:19 -0800

    Add functionality to deploy multiple Kubernetes jobs

--- push_docker.sh
+++ push_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 IMAGE_ID=$1
-docker tag $IMAGE_ID PeriscopeData/concourse-multiple-kube-deployments:latest
-docker push PeriscopeData/concourse-multiple-kube-deployments:latest
+docker tag $IMAGE_ID 490843488481.dkr.ecr.us-east-1.amazonaws.com/concourse-multiple-kube-deployments:latest
+docker push 490843488481.dkr.ecr.us-east-1.amazonaws.com/concourse-multiple-kube-deployments:latest
