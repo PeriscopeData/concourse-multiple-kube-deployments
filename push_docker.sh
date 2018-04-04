3952d9e6490b9e0a4204fd24479c008360f4fcd5
Shobhit Garg <gargshobhit90@gmail.com>
2018-04-03 20:15:06 -0700

    Now it builds AND pushes docker image

--- push_docker.sh
+++ push_docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-IMAGE_ID=$1
+response="$(docker build .)"
+IMAGE_ID=$(echo $response | awk 'NF>1{print $NF}')
+echo "******** Pushing docker image ID $IMAGE_ID ********"
 docker tag $IMAGE_ID 490843488481.dkr.ecr.us-east-1.amazonaws.com/concourse-multiple-kube-deployments:latest
 docker push 490843488481.dkr.ecr.us-east-1.amazonaws.com/concourse-multiple-kube-deployments:latest
